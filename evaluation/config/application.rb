8d2bf5f811d0cff877dc0b29bcaae285f52b48c4
Denise <meagher.denise@gmail.com>
2017-01-04 13:21:30 -0500

    add info for mailer

--- evaluation/config/application.rb
+++ evaluation/config/application.rb
@@ -11,5 +11,26 @@ module Evaluation
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    # config.action_mailer.raise_delivery_errors = true
+    # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+    # config.action_mailer.perform_deliveries = true
+    # config.action_mailer.delivery_method = :smtp
+    #
+    # config.action_mailer.smtp_settings = {
+    # :address        => 'smtp.sendgrid.net',
+    # :port           => '587',
+    # :authentication => :plain,
+    # :user_name      =>  ENV['SENDGRID_USERNAME'],
+    # :password       =>  ENV['SENDGRID_PASSWORD'],
+    # :domain         => 'localhost:3000',
+    # :enable_starttls_auto => true
+    # }
+
+    config.before_initialize do
+      dev = File.join(Rails.root, 'config', 'config.yml')
+      YAML.load(File.open(dev)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(dev)
+    end
   end
 end
