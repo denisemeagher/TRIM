aa09d977d092a08e94526908b6dcb881aa6255cf
Denise <meagher.denise@gmail.com>
2017-01-06 21:43:56 -0500

    editing email settings

--- app/mailers/notifier_mailer.rb
+++ app/mailers/notifier_mailer.rb
@@ -1,10 +1,10 @@
 class NotifierMailer < ApplicationMailer
   default :from => 'no-reply@example.com',
-          :subject => 'Apprentice Evaluation questionnaire results'
+          :subject => 'Apprentice Evaluation Questionnaire Results'
 
   def results_email(apprentice_evaluation)
     @apprentice_evaluation = apprentice_evaluation
     category 'SendGridRocks'
-    mail to: "meagher.denise@gmail.com"
+    mail to: "tim@trimagency.com"
   end
 end
